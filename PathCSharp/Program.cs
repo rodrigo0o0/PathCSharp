c9bf83bf96b613c38d4c7ba8635fe7ce715ffc7b
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-01 03:36:17 -0300

    first commit

---
+++ PathCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder\file1.txt ";
+            Console.WriteLine("GetDirectoryName : " + Path.GetDirectoryName(path));
+            Console.WriteLine("PathSeparator : " + Path.PathSeparator);
+            Console.WriteLine("DirectorySeparatorChar : " + Path.DirectorySeparatorChar);
+            Console.WriteLine("GetFileName : " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension : " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension : " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath : " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath : " + Path.GetTempPath());
+            Console.ReadKey();
+        }
+    }
+}
